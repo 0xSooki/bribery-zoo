4d6703add39671db02173a5da5e31ba43e992c7c
0xSooki <0xsooki@gmail.com>
2025-07-18 11:14:21 +0200

    add solidity-rlp-encode library

--- src/HeaderVerify.sol
+++ src/HeaderVerify.sol
@@ -1,66 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-library RLPEncoder {
-    function encodeBytes(bytes memory data) internal pure returns (bytes memory) {
-        if (data.length == 1 && uint8(data[0]) < 0x80) {
-            return data;
-        } else if (data.length <= 55) {
-            return abi.encodePacked(uint8(0x80 + data.length), data);
-        } else {
-            bytes memory lenBytes = toBytes(data.length);
-            return abi.encodePacked(uint8(0xb7 + lenBytes.length), lenBytes, data);
-        }
-    }
-
-    function encodeUint(uint256 value) internal pure returns (bytes memory) {
-        if (value == 0) {
-            return hex"80";
-        }
-        bytes memory data = toBytes(value);
-        return encodeBytes(data);
-    }
-
-    function encodeAddress(address addr) internal pure returns (bytes memory) {
-        return encodeBytes(abi.encodePacked(addr));
-    }
-
-    function encodeList(bytes[] memory items) internal pure returns (bytes memory) {
-        bytes memory encoded;
-        for (uint256 i = 0; i < items.length; i++) {
-            encoded = abi.encodePacked(encoded, items[i]);
-        }
-
-        if (encoded.length <= 55) {
-            return abi.encodePacked(uint8(0xc0 + encoded.length), encoded);
-        } else {
-            bytes memory lenBytes = toBytes(encoded.length);
-            return abi.encodePacked(uint8(0xf7 + lenBytes.length), lenBytes, encoded);
-        }
-    }
-
-    function toBytes(uint256 value) internal pure returns (bytes memory) {
-        if (value == 0) return new bytes(0);
-
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 256;
-        }
-
-        bytes memory result = new bytes(digits);
-        while (value != 0) {
-            digits--;
-            result[digits] = bytes1(uint8(value % 256));
-            value /= 256;
-        }
-        return result;
-    }
-}
+import {RLPEncode} from "lib/solidity-rlp-encode/contracts/RLPEncode.sol";
 
 contract HeaderVerify {
-    using RLPEncoder for *;
+    using RLPEncode for *;
 
     /**
      * @notice Block header structure for Cancun-era (post-Deneb) Ethereum blocks
@@ -115,29 +59,29 @@ contract HeaderVerify {
      */
     function encodeHeader(BlockHeader memory header) internal pure returns (bytes memory) {
         bytes[] memory items = new bytes[](21);
-        items[0] = RLPEncoder.encodeBytes(abi.encodePacked(header.parentHash));
-        items[1] = RLPEncoder.encodeBytes(abi.encodePacked(header.sha3Uncles));
-        items[2] = RLPEncoder.encodeAddress(header.miner);
-        items[3] = RLPEncoder.encodeBytes(abi.encodePacked(header.stateRoot));
-        items[4] = RLPEncoder.encodeBytes(abi.encodePacked(header.transactionsRoot));
-        items[5] = RLPEncoder.encodeBytes(abi.encodePacked(header.receiptsRoot));
-        items[6] = RLPEncoder.encodeBytes(header.logsBloom);
-        items[7] = RLPEncoder.encodeUint(header.difficulty);
-        items[8] = RLPEncoder.encodeUint(header.number);
-        items[9] = RLPEncoder.encodeUint(header.gasLimit);
-        items[10] = RLPEncoder.encodeUint(header.gasUsed);
-        items[11] = RLPEncoder.encodeUint(header.timestamp);
-        items[12] = RLPEncoder.encodeBytes(header.extraData);
-        items[13] = RLPEncoder.encodeBytes(abi.encodePacked(header.mixHash));
-        items[14] = RLPEncoder.encodeBytes(abi.encodePacked(header.nonce));
-        items[15] = RLPEncoder.encodeUint(header.baseFeePerGas);
-        items[16] = RLPEncoder.encodeBytes(abi.encodePacked(header.withdrawalsRoot));
-        items[17] = RLPEncoder.encodeUint(header.blobGasUsed);
-        items[18] = RLPEncoder.encodeUint(header.excessBlobGas);
-        items[19] = RLPEncoder.encodeBytes(abi.encodePacked(header.parentBeaconBlockRoot));
-        items[20] = RLPEncoder.encodeBytes(abi.encodePacked(header.requestsHash));
+        items[0] = RLPEncode.encodeBytes(abi.encodePacked(header.parentHash));
+        items[1] = RLPEncode.encodeBytes(abi.encodePacked(header.sha3Uncles));
+        items[2] = RLPEncode.encodeAddress(header.miner);
+        items[3] = RLPEncode.encodeBytes(abi.encodePacked(header.stateRoot));
+        items[4] = RLPEncode.encodeBytes(abi.encodePacked(header.transactionsRoot));
+        items[5] = RLPEncode.encodeBytes(abi.encodePacked(header.receiptsRoot));
+        items[6] = RLPEncode.encodeBytes(header.logsBloom);
+        items[7] = RLPEncode.encodeUint(header.difficulty);
+        items[8] = RLPEncode.encodeUint(header.number);
+        items[9] = RLPEncode.encodeUint(header.gasLimit);
+        items[10] = RLPEncode.encodeUint(header.gasUsed);
+        items[11] = RLPEncode.encodeUint(header.timestamp);
+        items[12] = RLPEncode.encodeBytes(header.extraData);
+        items[13] = RLPEncode.encodeBytes(abi.encodePacked(header.mixHash));
+        items[14] = RLPEncode.encodeBytes(abi.encodePacked(header.nonce));
+        items[15] = RLPEncode.encodeUint(header.baseFeePerGas);
+        items[16] = RLPEncode.encodeBytes(abi.encodePacked(header.withdrawalsRoot));
+        items[17] = RLPEncode.encodeUint(header.blobGasUsed);
+        items[18] = RLPEncode.encodeUint(header.excessBlobGas);
+        items[19] = RLPEncode.encodeBytes(abi.encodePacked(header.parentBeaconBlockRoot));
+        items[20] = RLPEncode.encodeBytes(abi.encodePacked(header.requestsHash));
 
-        return RLPEncoder.encodeList(items);
+        return RLPEncode.encodeList(items);
     }
 
     /**
--- test/HeaderVerify.t.sol
+++ test/HeaderVerify.t.sol
@@ -20,34 +20,34 @@ contract HeaderVerifyTest is Test {
      * @notice Single test to verify computed hash matches expected hash for block 22916101
      * @dev This test uses real mainnet block 22916101 data to ensure hash computation is correct
      */
-    function testVerifyBlockhash() public {
+    function testVerifyBlockhash() public view {
         // Real mainnet block 22916101 header data
         HeaderVerify.BlockHeader memory header = HeaderVerify.BlockHeader({
-            parentHash: 0x71f36f0edbffb49d3885ac4e311dfb61088945b6f5a0797a8038dd3764821424,
+            parentHash: 0x7717e103fab2d799f8e4106d4554e5132ca9ad5156f6f16fa7d21c767256ba4f,
             sha3Uncles: 0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347,
-            miner: 0x7DbfeD5686847113b527DC215DBA4E332DF8cc6c,
-            stateRoot: 0x5f73b9a9f02e7e47c1c87ddfbf06f9e84ff03fc9b227a15a749748f8154677bf,
-            transactionsRoot: 0x16849e354a35712b40ee99e96288f8b36fec8005db4e0cd7b240bee52743520d,
-            receiptsRoot: 0x5599b4da2a7208958b0392990c3c14ba297223110f16b62863daa4d1d8dd29f7,
-            logsBloom: hex"11fe39f049c0887a36e5045ddae29dd173d498b944c431591a3b0410e62e8887128c491d90c842274f252b331d1613b1c7e3d84088a420007bcd3ec029ae69dac9ce1848efbcf30ee941636e9e44b1a008641dbf176c68a4e07e986c8d6010174ee3f16c0b762c22a66dad9058c44a11db1d9e402dd5a647b71ac1d6531f780a3e3aaf48bccbc97f78d8046803236726b10e9d717b902caa8e6fa9d368b83568174638fe01f0b212829536de661823ba30645205a421d7088b6f3c73114829f57f6787126a581f132489536c02eb1a262915264205ac997143d3e7cebc04224f4452acc4a514e09d832e96a50ac358d36f29f853415442c7660199e2116d4527",
+            miner: 0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97,
+            stateRoot: 0xb6a6a4f21cb3732b89690b552d5c39c718dd43275eecb0f9167bd71bb8e2f7ce,
+            transactionsRoot: 0x333b983a945c9ace9d3c58021ac4e911d054c4bc912d705d1d11426be44619dc,
+            receiptsRoot: 0x6c942cc4d783951e52e52c11b2fb17e0574af3c664d66170521a93d80aff9a38,
+            logsBloom: hex"81ea2be709f579def272335384189cb09b47851dc4f6d0ee31f76e9126f1863bd55cafb6ea4ea0fefe727bd76f0f9399df2cf2b49d9b6cf247e3bcd1292e63a5722495b9614f9f0dce8f792e92c132e29ece37b361474e6038337c62a82407d04c7de81cebef06f67454d5e27f656fbfb1379bfe8710cc4dd7387a94a27fd69f6926a3b91065cf39485df74d3f366b7646998f5515937b59fc7bd7f745b57efeabdc31ea53f07dc00ef21fc67f57cede22c65ebfcfbb5459cc7f65df7935587afd977e5a44c82be9f16fb8d65b62fd556de5645fcf7894d41edf7152f1f0fb7bf3f33c0195b2adf091e4f2ecf1f13be5ff4dba22511c9ec7d4b9d7d298ef5627",
             difficulty: 0,
-            number: 22916101,
-            gasLimit: 36140617,
-            gasUsed: 12169751,
-            timestamp: 1752480215,
-            extraData: hex"4275696c6465722b207777772e627463732e636f6d2f6275696c646572",
-            mixHash: 0x6f74c123e3532e124b6a263c3755c163bc6940f04feec2b090f4aadec90af618,
+            number: 0x15d9085, // 22909061 in decimal
+            gasLimit: 0x2277537,
+            gasUsed: 0x11437cb,
+            timestamp: 0x68736e03,
+            extraData: hex"546974616e2028746974616e6275696c6465722e78797a29",
+            mixHash: 0x1594d24382ef583d1e1be9412b4a59ee3b401debf5cba382b824ef07110b64fa,
             nonce: 0x0000000000000000,
-            baseFeePerGas: 1716818952,
-            withdrawalsRoot: 0xd6e43380ed12db043113c66feae84e345b46a059feabeabad78d1dab54261cbc,
-            blobGasUsed: 131072,
-            excessBlobGas: 393216,
-            parentBeaconBlockRoot: 0xec61dae56c8fdba45cdf7232a3cb0ca7d2e9ba2e8fabe6f9df3a2c0bd6b5ad3f,
+            baseFeePerGas: 0x167675bd,
+            withdrawalsRoot: 0x1f0186dfc6bef700c3fb9d867d6cc49c1cf1be4bbc0505796a0b46886db67b8d,
+            blobGasUsed: 0x0,
+            excessBlobGas: 0x20000,
+            parentBeaconBlockRoot: 0xce765cd034ba2f51e2c91a4111ec40b7b0fdd6dbc0996b58b5e35e3ff8b6ed43,
             requestsHash: 0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
         });
 
         // Expected hash for block 22916101
-        bytes32 expectedHash = 0x860630231fb0e55d713ad11ab0bd79ba09dcf2017ecc5aaf43206a097b765ec2;
+        bytes32 expectedHash = 0x23254aeb7f8e36fa7baa8ea2df34c8a3896a6c31dce92546adc7b9598d4ae590;
 
         // Get computed hash from contract
         bytes32 computedHash = headerVerify.getComputedHash(header);
